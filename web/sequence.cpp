5a573e7f4b096df1b1f9ec3bfc36dd5f7680ea64
Ingun Jon <ingun37@gmail.com>
2026-01-09 15:26:16 +0900

    basic system

--- web/sequence.cpp
+++ web/sequence.cpp
@@ -1,86 +1,58 @@
 #include <emscripten/html5.h>
 #include <iostream>
+#include <utility>
 #include <nvrhi/nvrhi.h>
 #include <GLFW/glfw3.h>
 #include <webgpu/webgpu_cpp.h>
 #include <emscripten/bind.h>
 
-struct Context {
-    nvrhi::DeviceHandle nvrhiDevice;
-
-    Context() = delete;
-
-    Context(
-        nvrhi::DeviceHandle nvrhiDevice)
-        : nvrhiDevice(std::move(nvrhiDevice)) {
-    }
-};
 
 struct App {
     virtual ~App() = default;
 
-    const Context& context;
     const std::string title;
 
-    App(const Context& webGpu, std::string title)
-        : context(webGpu),
-          title(std::move(title)) {
+    App(std::string title)
+        : title(std::move(title)) {
     }
 
-    virtual std::unique_ptr<App> run() =0;
+    virtual void run(std::function<void(std::unique_ptr<App> app_ptr)>) =0;
 };
 
 using AppPtr = std::unique_ptr<App>;
+
 
 using namespace emscripten;
 static std::string g_input;
 
 class AppRunner {
-    Context ctx;
     AppPtr app;
 
 public:
     AppRunner() = delete;
 
-    AppRunner(Context&& ctx, AppPtr app)
-        : ctx(std::move(ctx)),
-          app(std::move(app)) {
+    AppRunner(AppPtr app)
+        : app(std::move(app)) {
     }
 
     void run() {
-        app = app->run();
+        std::cout << "Running: " << app->title << std::endl;
+        app->run([this](AppPtr new_app) {
+            this->app = std::move(new_app);
+        });
     }
 };
 
 enum Stage { Wait, Next };
 
 struct UserData {
-    GLFWwindow* window;
-    wgpu::Instance* instance;
-    int width;
-    int height;
-    wgpu::Adapter* adapter = nullptr;
-    wgpu::Device* device = nullptr;
-    wgpu::Queue* queue = nullptr;
-    nvrhi::DeviceHandle* nvrhi_device = nullptr;
-    wgpu::Surface* surface = nullptr;
-    nvrhi::ShaderHandle* vertex_shader;
-    nvrhi::ShaderHandle* pixel_shader;
-    nvrhi::BufferHandle* random_buffer;
-    nvrhi::BufferHandle* buffer;
-    int count = 0;
-    const void* map_ptr = nullptr;
+    AppRunner* app_runner;
 
     UserData() = delete;
 
-    explicit UserData(GLFWwindow* window,
-                      wgpu::Instance* instance,
-                      int width,
-                      int height)
-        : window(window),
-          instance(instance),
-          width(width),
-          height(height) {
+    explicit UserData(
+        AppRunner* app_runner)
+        : app_runner(app_runner) {
     }
 };
 
@@ -88,6 +60,7 @@ void _iter(UserData& user_data) {
     if (g_input == "") {
     } else {
         std::cout << "input:" << g_input << std::endl;
+        user_data.app_runner->run();
         g_input = "";
     }
 }
@@ -110,6 +83,71 @@ void run_next_app() {
 EMSCRIPTEN_BINDINGS(my_module) {
     function("run_next_app", &run_next_app);
 }
+
+struct InitDevice : public App {
+    const wgpu::Adapter& adapter;
+
+    InitDevice() = delete;
+
+    InitDevice(const wgpu::Adapter& adapter)
+        : App("Init Device"),
+          adapter(adapter) {
+    }
+
+    void run(std::function<void(std::unique_ptr<App> app_ptr)> callback) override {
+        wgpu::DeviceDescriptor deviceDesc = {};
+        deviceDesc.nextInChain = nullptr;
+        std::cout << "Requesting device" << std::endl;
+        adapter.RequestDevice(
+            &deviceDesc,
+            wgpu::CallbackMode::AllowSpontaneous,
+            [cb = std::move(callback)](wgpu::RequestDeviceStatus status,
+                                       wgpu::Device dv,
+                                       wgpu::StringView message) {
+                if (status != wgpu::RequestDeviceStatus::Success) {
+                    std::cerr << "Failed to get an device: " << message.data;
+                    throw std::runtime_error("Failed to get an device");
+                }
+                std::cout << "Device is created" << std::endl;
+                throw std::runtime_error("Device is created");
+                // cb(std::move(dv));
+            });
+    }
+};
+
+struct InitApp : public App {
+    wgpu::Instance instance;
+
+    InitApp() = delete;
+
+    InitApp(wgpu::Instance&& instance)
+        : App("Init Adapter"),
+          instance(instance) {
+    }
+
+    void run(std::function<void(std::unique_ptr<App> app_ptr)> callback) override {
+        std::cout << "Requesting adapter" << std::endl;
+        wgpu::RequestAdapterOptions adapterOptions = {};
+
+        adapterOptions.nextInChain = nullptr;
+        adapterOptions.backendType = wgpu::BackendType::WebGPU;
+
+        instance.RequestAdapter(
+            &adapterOptions,
+            wgpu::CallbackMode::AllowSpontaneous,
+            [cb = std::move(callback)](wgpu::RequestAdapterStatus status,
+                                       wgpu::Adapter adapter,
+                                       wgpu::StringView message) {
+                if (status != wgpu::RequestAdapterStatus::Success) {
+                    std::cerr << "Failed to get an adapter: " << message.data;
+                    return;
+                } else {
+                    std::cout << "Adapter is created" << std::endl;
+                    cb(std::make_unique<InitDevice>(std::move(adapter)));
+                }
+            });
+    }
+};
 
 int main() {
     GLFWwindow* window;
@@ -135,7 +173,7 @@ int main() {
     /* Loop until the user closes the window */
     emscripten_request_animation_frame_loop(
         one_iter,
-        new UserData(window, &instance, width, height));
+        new UserData(new AppRunner(std::make_unique<InitApp>(std::move(instance)))));
 
     glfwTerminate();
     return 0;
