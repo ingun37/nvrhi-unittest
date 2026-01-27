1ae8e697e241b00520c66c4456a1d8b5014f2e96
Ingun Jon <ingun37@gmail.com>
2026-01-27 17:38:12 +0900

    improve message

--- native/scenario/scenario.cpp
+++ native/scenario/scenario.cpp
@@ -9,10 +9,16 @@
 #include "ChooseApp.h"
 
 void run_app(Context&& ctx) {
+#if defined(EMSCRIPTEN)
+    throw std::runtime_error("run_app breaks in Emscripten. Use the scenario runner for emscripten in /web/.");
+#endif
     AppPtr app_p = create_app_immediately<ChooseApp>(ctx);
     while (app_p != nullptr) {
         auto app = app_p->get_future().get();
-        if (app == nullptr) break;
+        if (app == nullptr) {
+            std::cout << "NULL is (lazily) returned as the next app. Exiting..." << std::endl;
+            break;
+        }
         std::cout << app->title << std::endl;
         std::cout << app->prompt << std::endl;
         std::cout << "Default value: " << app->defaultInput << std::endl;
@@ -20,5 +26,10 @@ void run_app(Context&& ctx) {
         std::string input;
         std::getline(std::cin, input);
         app_p = app->run(input.empty() ? app->defaultInput : input);
+
+        if (app_p == nullptr) {
+            std::cout << "NULL is returned as the next app. Exiting..." << std::endl;
+            break;
+        }
     }
 }
